b78a18793e42f7c33aa0d726746478843df57980
Richi Rodriguez <rrodr420@fiu.edu>
2022-05-19 11:57:17 -0600

    Added ToIntVector2()

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class Maze : MonoBehaviour
 {
-    public int sizeX, sizeZ;
     public MazeCell cellPrefab;
     private MazeCell[,] cells;
     public float generationStepDelay;
@@ -15,13 +14,20 @@ public class Maze : MonoBehaviour
     {
         WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
         cells = new MazeCell[size.x, size.z];
-        for (int x = 0; x < size.x; x++)
+        IntVector2 coordinates = RandomCoordinates;
+        //for (int x = 0; x < size.x; x++)
+        //{
+        //    for (int z = 0; z < size.z; z++)
+        //    {
+        //        yield return delay;
+        //        CreateCell(new IntVector2(x, z));
+        //    }
+        //}
+        while (ContainsCoordinate(coordinates))
         {
-            for (int z = 0; z < size.z; z++)
-            {
-                yield return delay;
-                CreateCell(new IntVector2(x, z));
-            }
+            yield return delay;
+            CreateCell(coordinates);
+            coordinates.z += 1;
         }
     }
 
@@ -33,5 +39,18 @@ public class Maze : MonoBehaviour
         newCell.name = $"Maze Cell {coordinates.x}, {coordinates.z}";
         newCell.transform.parent = transform;
         newCell.transform.localPosition = new Vector3(coordinates.x - (size.x * 0.5f) + 0.5f, 0f, coordinates.z - (size.z * 0.5f) + 0.5f);
+    }
+
+    public IntVector2 RandomCoordinates
+    {
+        get
+        {
+            return new IntVector2(UnityEngine.Random.Range(0, size.x), UnityEngine.Random.Range(0, size.z));
+        }
+    }
+
+    public bool ContainsCoordinate(IntVector2 coordiante)
+    {
+        return coordiante.x >= 0 && coordiante.x < size.x && coordiante.z >= 0 && coordiante.z < size.z;
     }
 }
